08177e0d58ead0153f58fc6cafdf08830b57e75b
MSR-layer <marah44413@gmail.com>
2022-02-04 21:39:28 +0300

    evaluate function

--- script.js
+++ script.js
@@ -1,42 +1,93 @@
+const operandList = [
+    '+',
+    '÷',
+    '-',
+    '×'
+];
 
-function displayContent(){
-    const screen = document.querySelector('#calc-screen');
-    screen.textContent += this.textContent;
+const screen = document.querySelector('#equation');
+
+function displayContent(){ 
+    if(screen.textContent.endsWith('.')) 
+        screen.textContent += `${this.textContent}`;
+    else
+        screen.textContent += ` ${this.textContent}`;
 }
 
 function clearScreen(){
     hasDP = 0;
-    const screen = document.querySelector('#calc-screen');
+    hasOperand = 0; 
     screen.textContent = '';
 }
 
 function deleteChar(){
-    const screen = document.querySelector('#calc-screen');
+    
     if(screen.textContent.endsWith('.')) hasDP = 0;
-    screen.textContent = screen.textContent.slice(0, -1);
+    else if(operandList.some(o => screen.textContent.endsWith(o))) hasOperand = 0;
+    screen.textContent = screen.textContent.slice(0, -2);
 }
 
 let hasDP = 0;
 function addDecimalPoint(){
-    const screen = document.querySelector('#calc-screen');
-    if(!hasDP){
+    if(!hasDP && !operandList.some(o => screen.textContent.endsWith(o))){
         hasDP = 1;
         screen.textContent += '.'; 
     }
 
 }
+
+hasOperand = 0;
+function addOperand(){
+    if(!hasOperand /*&& !screen.textContent.endsWith('.')*/){
+        hasOperand = 1;
+        hasDP = 0;
+        screen.textContent += ` ${this.textContent}`; 
+    }else if(hasOperand && operandList.some(o => screen.textContent.endsWith(o))){
+        screen.textContent = screen.textContent.slice(0, -2);
+        screen.textContent += ` ${this.textContent}`
+    }
+}
+
+function evaluate(){
+    const str = screen.textContent;
+    console.log(str);
+    const obj = str.split(' ');
+    obj.shift();
+    console.log(obj);
+
+    a = parseFloat(obj[0]);
+    b = parseFloat(obj[2]);
+
+    let result;
+    if(obj[1] === '+') 
+        result = (a + b);
+    if(obj[1] === '-')
+        result = (a - b);
+    if(obj[1] === '×')
+        result = (a * b);
+    if(obj[1] === '÷')
+        result = (a / b);
+
+    document.querySelector('#result').textContent = result;
+}
+
 const numButtons = Array.from(document.querySelectorAll('#calc-buttons .num.input'));
 const operandButtons = Array.from(document.querySelectorAll('#calc-buttons .operand.input'));
 const clearButton = document.querySelector('#clear');
 const deleteButton = document.querySelector('#delete');
 const decimalButton = document.querySelector('#dp');
+const equalButton = document.querySelector('#equal');
+
+clearScreen();
 
 numButtons.forEach(key => key.addEventListener('click', displayContent));
 
-operandButtons.forEach(key => key.addEventListener('click', displayContent));
+operandButtons.forEach(key => key.addEventListener('click', addOperand));
 
 clearButton.addEventListener('click', clearScreen);
 
 deleteButton.addEventListener('click', deleteChar);
 
 decimalButton.addEventListener('click', addDecimalPoint);
+
+equalButton.addEventListener('click', evaluate);
